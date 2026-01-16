bc372eeb1aa892ca0d44e883aba399838b9c53e5
prafullvk18 <patilprafull15@gmail.com>
2026-01-16 17:22:21 +0530

    thrid commit

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -10,7 +10,7 @@ test('login page testcase', async ({ page }) => {
     await page.locator("//input[@id='username']").fill("student");
     await page.waitForTimeout(3000);
     await page.locator("//input[@id='password']").fill("Password123");
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(2500);
     await page.locator("//button[@id='submit']").click;
     await page.waitForTimeout(4000);
 })
